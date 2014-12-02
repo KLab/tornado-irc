200139d833c929b7e647d058e9f369160007cc48
INADA Naoki <inada-n@klab.com>
2014-12-02 23:02:41 +0900

    Add tornado to requirements.

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="tornado-irc",
     py_modules=['tornado_irc'],
     version='0.0.1',
+    requires=["tornado"],
     classifiers=[
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
