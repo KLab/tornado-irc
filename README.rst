913abac265104ae00384831ceea40333819c10ad
INADA Naoki <inada-n@klab.com>
2014-12-03 14:03:01 +0900

    Add README

---
+++ README.rst
@@ -0,0 +1,9 @@
+tornado-irc
+============
+
+tornado-irc is IRC Client for Tornado.
+
+requirements:
+
+    * Python >= 3.3
+    * Tornado >= 4.0
--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup
 
+with open('README.rst', 'r', encoding='utf-8') as f:
+    long_description=f.read()
+
 setup(
     name="tornado-irc",
-    py_modules=['tornado_irc'],
     version='0.0.1',
+    description="IRC Client for Tornado",
+    long_description=long_description,
+    author="INADA Naoki",
+    author_email="songofacandy@gmail.com",
     requires=["tornado"],
+    py_modules=['tornado_irc'],
     classifiers=[
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
